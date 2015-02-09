90afea08184d5346314daa2127c0724de4109504
MrSoulReaver <MrSoulReaver@gmail.com>
2015-02-09 19:18:43 +0200

    Check_ping

---
+++ test_host.py
@@ -0,0 +1,26 @@
+# !/user/bin/python
+# -*- coding: utf-8 -*-
+import time
+import subprocess
+
+def recconect():
+    for ip in ip_list:
+        ping = subprocess.call(["ping", ip])
+        if ping == 0:
+            time.sleep(30)
+            print("UP") #rdectop reconect
+            return ping
+        else:
+            time.sleep(5)
+            recconect()
+
+ip_list = ["Host"]
+while 1:
+    for ip in ip_list:
+        ping = subprocess.call(["ping", ip])
+        if ping != 0:
+            print("Down")
+            recconect()
+        else:
+            print("Up")
+            time.sleep(30)
